43f32a18f810860d5f055df8620a670e065a2a83
Himani Suresh Rothey <himanirothey200@gmail.com>
2023-09-18 00:05:58 +0530

    first commit

---
+++ vendor/air-datepicker/locale/cs.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  days: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota'],
+  daysShort: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'],
+  daysMin: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'],
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  today: 'Dnes',
+  clear: 'Vymazat',
+  dateFormat: 'dd.MM.yyyy',
+  timeFormat: 'HH:mm',
+  firstDay: 1
+};
+exports["default"] = _default;
